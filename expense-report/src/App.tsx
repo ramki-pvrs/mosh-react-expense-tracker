3f3bb00d9e6a4a7180e353c279944e032883a30a
Ramakrishnan Srinivasan <ramki.pvrs@gmail.com>
2024-01-01 19:01:09 +0530

    working till expense report with console.logged Delete

--- expense-report/src/App.tsx
+++ expense-report/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const expenses = [
+    { id: 1, description: "aaa", amount: 10, category: "Utils" },
+    { id: 2, description: "bbb", amount: 20, category: "Utils" },
+    { id: 3, description: "ccc", amount: 30, category: "TV" },
+    { id: 4, description: "ddd", amount: 40, category: "TV" },
+    { id: 5, description: "eee", amount: 50, category: "Mobile" },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p> Init </p>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("delete req on expense id=", id)}
+      ></ExpenseList>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ expense-report/src/expense-tracker/components/ExpenseList.tsx
@@ -0,0 +1,79 @@
+//each expense row is one object
+//like that you may have multiple expense rows so Expense objects list
+//so two interfaces, first one is single expense obj
+//second is list of expense object
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string; //in real-world category will be another object with id and name properties
+}
+
+interface Props {
+  expenses: Expense[];
+  //this is the call-back to delete the expense object from parent component, given id
+  //from specific row in this component
+  onDelete: (id: number) => void;
+}
+
+function ExpenseList({ expenses, onDelete }: Props) {
+  return (
+    <table className="table table-border">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          {/*the next th is the one where Delete button will come; leave empty */}
+          <th></th>
+        </tr>
+      </thead>
+      {/* In tbody you display expenses of individual items; meaning your data rows */}
+      {/* so thats a variable; so you dont want to hard code tbody rows */}
+      {/* tbody rows contents are passed as props (function params) */}
+      {/* so you need interfaces above this component function definition */}
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              {/*REMEMBER: state change should happen in a component which holds the state */}
+              {/*expense state is props here, passed through an interface that means */}
+              {/*when user clicks on delete, expense is NOT DELETED in this component but */}
+              {/* a call-back function is used to send the message back to parent, may be APP.tsx */}
+              {/*to inform so that parent can delete the expense (or pass it further in the chain in the app may be) */}
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(expense.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          {/*We need to calc total dynamically on the footer row;  */}
+          {/* in Js arrays (objects) has reduce method, you pass an array to reduce and can handle sum of all expenses from there*/}
+          {/* reduce method has arry function with 2 params; acc for accumulator and individual item; */}
+          {/* 0 is default for accumulate which we need to pass */}
+          <td>
+            ${expenses
+              .reduce((acc, expense) => expense.amount + acc, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+}
+
+export default ExpenseList;
